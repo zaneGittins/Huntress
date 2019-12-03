5791f3fbea1143c1d610dc068caace605aac28f6
Zane Gittins <zgittins@haascnc.com>
2019-12-03 09:21:31 -0800

    StartupPrograms now attempts to calculate SHA256 hash.

--- modules/StartupPrograms.ps1
+++ modules/StartupPrograms.ps1
@@ -17,18 +17,20 @@ class StartupProgram {
     [string]$Name     = ""
     [string]$Location = ""
     [string]$Command  = ""
+    [string]$Hash     = ""
 }
 
 $AllStartupPrograms = Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User
 
 foreach($CurrentStartupProgram in $AllStartupPrograms) {
 
-    $NewStartupProgram          = [RecentDoc]::new()
+    $NewStartupProgram          = [StartupProgram]::new()
     $NewStartupProgram.User     = $CurrentStartupProgram.User
     $NewStartupProgram.Name     = $CurrentStartupProgram.Name
     $NewStartupProgram.Location = $CurrentStartupProgram.Location
     $NewStartupProgram.Command  = $CurrentStartupProgram.Command
-    $global:ReturnData          += $CurrentStartupProgram
+    $NewStartupProgram.Hash     =  (Select-String -InputObject $NewStartupProgram.Command -Pattern "\`"[\w\W]+?\`"" -AllMatches | % { $_.Matches } | % { Get-FileHash $_.Value.Replace("`"","") }).Hash
+    $global:ReturnData          += $NewStartupProgram
 
 }
 
