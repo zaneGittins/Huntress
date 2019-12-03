01f37225b32385cde345685794c60f5ac8b91cc5
Zane Gittins <zgittins@haascnc.com>
2019-12-03 09:31:06 -0800

    Calculates the signature of startup programs.

--- modules/StartupPrograms.ps1
+++ modules/StartupPrograms.ps1
@@ -13,23 +13,31 @@ $ErrorActionPreference = "SilentlyContinue"
 $global:ReturnData = @()
 
 class StartupProgram {
-    [string]$User     = ""
-    [string]$Name     = ""
-    [string]$Location = ""
-    [string]$Command  = ""
-    [string]$Hash     = ""
+    [string]$User               = ""
+    [string]$Name               = ""
+    [string]$Location           = ""
+    [string]$Command            = ""
+    [string]$Hash               = ""
+    [string]$SignatureSubject   = ""
+    [string]$SignatureStatus    = ""
 }
 
 $AllStartupPrograms = Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User
 
 foreach($CurrentStartupProgram in $AllStartupPrograms) {
 
-    $NewStartupProgram          = [StartupProgram]::new()
-    $NewStartupProgram.User     = $CurrentStartupProgram.User
-    $NewStartupProgram.Name     = $CurrentStartupProgram.Name
-    $NewStartupProgram.Location = $CurrentStartupProgram.Location
-    $NewStartupProgram.Command  = $CurrentStartupProgram.Command
-    $NewStartupProgram.Hash     =  (Select-String -InputObject $NewStartupProgram.Command -Pattern "\`"[\w\W]+?\`"" -AllMatches | % { $_.Matches } | % { Get-FileHash $_.Value.Replace("`"","") }).Hash
+    $NewStartupProgram                  = [StartupProgram]::new()
+    $NewStartupProgram.User             = $CurrentStartupProgram.User
+    $NewStartupProgram.Name             = $CurrentStartupProgram.Name
+    $NewStartupProgram.Location         = $CurrentStartupProgram.Location
+    $NewStartupProgram.Command          = $CurrentStartupProgram.Command
+    $ProgramPath                        = (Select-String -InputObject $NewStartupProgram.Command -Pattern "[\w\W]+?\.exe" -AllMatches | % { $_.Matches } | % { $_.Value.Replace("`"","") })
+    $NewStartupProgram.Hash             = (Get-FileHash -Path $ProgramPath).Hash 
+    $Signature                          = (Get-AuthenticodeSignature $ProgramPath)
+    $NewStartupProgram.SignatureStatus  = $Signature.Status 
+    $NewStartupProgram.SignatureSubject = $Signature.SignerCertificate.Subject
+
+
     $global:ReturnData          += $NewStartupProgram
 
 }
